a2c4ce01d3e43d9c7f218c65036416540ab69723
basilaslam <basilaslamnp@gmail.com>
2022-12-12 17:39:43 +0530

    converted to vite

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <Row title='Romance Movies'fetchUrl={requests.fetchRomanceMovies}/>
       <Row title='Documentaries'fetchUrl={requests.fetchDocumentaries}/>
 
-    </div>
+    </div> 
   );
 }
 
